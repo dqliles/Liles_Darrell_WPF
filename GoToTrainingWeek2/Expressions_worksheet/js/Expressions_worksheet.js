453066f436d705bac5539fc3e92bf8a1c38dbfd8
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-08 13:20:53 -0400

    Expression_worksheet
    setup given and tested it out in console

--- GoToTrainingWeek2/Expressions_worksheet/js/Expressions_worksheet.js
+++ GoToTrainingWeek2/Expressions_worksheet/js/Expressions_worksheet.js
@@ -1,6 +1,14 @@
 //Expressions_worksheet
 
 
+//Calculate how old Sparky the pit bull is in dog years based on his actual age
+
+
+
+//this is the setup of the given
+var sparkyAge = 3;
+console.log(sparkyAge);
+
 
 
 
