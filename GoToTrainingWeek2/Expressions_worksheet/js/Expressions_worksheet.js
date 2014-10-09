a4a78f28a216114e3e66d075bfbe6081e17490a4
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-09 14:58:32 -0400

    files setup for Expressions_personal
    files setup for Expressions_personal

--- GoToTrainingWeek2/Expressions_worksheet/js/Expressions_worksheet.js
+++ GoToTrainingWeek2/Expressions_worksheet/js/Expressions_worksheet.js
@@ -1,18 +1,48 @@
+
+
+
+//Darrell Liles
+//Oct 08 2014
 //Expressions_worksheet
 
 
 //Calculate how old Sparky the pit bull is in dog years based on his actual age
 
 
-
 //this is the setup of the given
 var sparkyAge = 3;
+//I tested the setup of the given in the console to see if it works
 console.log(sparkyAge);
 
 //this is the setup of my result variable
 var ageInDogYears = 7 * sparkyAge;
+//I tested the setup of my result variable in the console to see if it works
 console.log(ageInDogYears);
 
+//This is the result to print
+console.log("Sparky is 3 human years old which is 21 dog years");
 
 
-alert("Testing 1, 2, 3!");
+
+
+
+//Calculate how much pizza each partygoer will get at the party
+
+
+//This is the setup of the given variables
+var numberOfSlicesPerPizza = 8;
+var numberOfPeopleAtTheParty = 10;
+var numberOfPizzasOrdered = 5;
+
+// I tested the setup of one of the given in the console
+console.log(numberOfPeopleAtTheParty);
+
+
+var slicesForEach = numberOfSlicesPerPizza * numberOfPizzasOrdered / numberOfPeopleAtTheParty;
+console.log(slicesForEach);
+
+//This is the result printed to the console
+console.log("Each person ate 4 slices of pizza at the party");
+
+
+//alert("Testing 1, 2, 3!");
