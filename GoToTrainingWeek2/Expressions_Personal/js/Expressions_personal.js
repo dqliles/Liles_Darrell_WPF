dad2a15da031a70a789856c9cad2c9d597c06e1b
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-09 18:18:56 -0400

    gave the user the cost of tickets
    Gave the user the cost of tickets and printed the result to the console

--- GoToTrainingWeek2/Expressions_Personal/js/Expressions_personal.js
+++ GoToTrainingWeek2/Expressions_Personal/js/Expressions_personal.js
@@ -2,16 +2,17 @@
  /*
 Darrell Liles
 0ct 09 2014
-Expressions
+Expressions_Personal
 */
 
 //Calculation for the cost of movie tickets for a family
 
 
 //set up for the given variable
-var adultTicket = 8.00;
-var childTicket = 6.00;
-var studentTicket = 6.50;
+var adultTicket = 8;
+var childTicket = 6;
+var studentTicket = 5;
+
 //I tested out the givens in the console to see if they were setup properly//
 console.log(adultTicket);
 console.log(childTicket);
@@ -29,9 +30,17 @@ var child = prompt("Now enter the number of children");
 console.log(child);
 var students = prompt("Please enter the number of students ");
 console.log (students);
+
 //Created calculation using the givens and the info collected from prompts//
  var costOfTickets = (adultTicket * adults + childTicket * child + studentTicket * students);
-//Tested calculation in console
+
+//Tested calculation in console//
  console.log(costOfTickets);
+
+//alert the user of the cost for tickets//
+alert("Ok  "+name+" ! The cost of your tickets will be  "+costOfTickets+" dollars.");
+
+//printed the cost in the console//
+console.log("Ok  "+name+" ! The cost of your tickets will be  "+costOfTickets+" dollars.");
 
 //alert("Testing 1, 2, 3!");
