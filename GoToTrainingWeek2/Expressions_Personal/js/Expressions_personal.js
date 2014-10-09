528d34ce280613a9e3e091ac5265326ddc232421
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-09 15:21:16 -0400

    setup givens for Expression_personal
    setup givens for Expression_personal

---
+++ GoToTrainingWeek2/Expressions_Personal/js/Expressions_personal.js
@@ -0,0 +1,18 @@
+
+ /*
+Darrell Liles
+0ct 09 2014
+Expressions
+*/
+
+//Calculation for the cost of movie tickets for a family
+
+
+ //set up for the given variable
+ var adultTicket = 8;
+ var childTicket = 6;
+ var studentTicket = 6.5
+
+
+
+//alert("Testing 1, 2, 3!");
