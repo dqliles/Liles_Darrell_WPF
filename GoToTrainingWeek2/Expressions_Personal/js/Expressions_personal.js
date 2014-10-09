75d20d495c89f47f746286e49d1a68d8d01b551a
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-09 15:37:07 -0400

    Expressions_personal
    tested out  givens in print console

--- GoToTrainingWeek2/Expressions_Personal/js/Expressions_personal.js
+++ GoToTrainingWeek2/Expressions_Personal/js/Expressions_personal.js
@@ -8,11 +8,13 @@ Expressions
 //Calculation for the cost of movie tickets for a family
 
 
- //set up for the given variable
- var adultTicket = 8;
- var childTicket = 6;
- var studentTicket = 6.5
-
-
+//set up for the given variable
+var adultTicket = 8;
+var childTicket = 6;
+var studentTicket = 6.5;
+//I tested out the givens in the console to see if they were setup properly//
+console.log(adultTicket);
+console.log(childTicket);
+console.log(studentTicket);
 
 //alert("Testing 1, 2, 3!");
