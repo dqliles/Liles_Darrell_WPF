3ad645e1f61a908284ca40983f047b2fadbd4445
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-09 21:41:41 -0400

    created variable for array
    created variable to store value of each array

--- GoToTrainingWeek2/Expressions/Expressions_Industry/js/Expressions_industry.js
+++ GoToTrainingWeek2/Expressions/Expressions_Industry/js/Expressions_industry.js
@@ -12,6 +12,12 @@ Expressions_industry
 //created my given using an array//
 var fruitBasket = ["grapes" + "pears"];
 
+//created variable to store value of each array//
+var grapes = 10;
+var pears = 10;
+
+//tested out variable in the console
+console.log(grapes);
 
 
 //alert("Testing 1, 2, 3!");
