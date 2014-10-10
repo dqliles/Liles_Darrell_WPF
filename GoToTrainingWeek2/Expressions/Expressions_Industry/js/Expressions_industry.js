38a9bd7906f108a9788ef972be22ba6da92d7490
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-09 22:19:54 -0400

    set up a prompt
    use prompt to explain to user why we need some of their info

--- GoToTrainingWeek2/Expressions/Expressions_Industry/js/Expressions_industry.js
+++ GoToTrainingWeek2/Expressions/Expressions_Industry/js/Expressions_industry.js
@@ -7,17 +7,21 @@ Expressions_industry
 
 
 
- //calculate how many snacks were leftover during snack time.
+ //calculate how many grapes and pears were leftover during snack time with the attendance of twenty children .
 
 //created my given using an array//
 var fruitBasket = ["grapes" + "pears"];
 
 //created variable to store value of each array//
-var grapes = 10;
+var grapes = 20;
 var pears = 10;
 
 //tested out variable in the console
 console.log(grapes);
+
+//Prompt to explain to user why we need some input from them
+Prompt("hello" + "Lets calculate how many grapes and pears were left over from snack time"
+
 
 
 //alert("Testing 1, 2, 3!");
