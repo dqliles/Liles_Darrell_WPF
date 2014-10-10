1c2c8504fa2829ad22280711d728c4421b486bed
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-10 01:31:08 -0400

    fixed errors in alert
    fixed errors in alert

--- GoToTrainingWeek2/Expressions/Expressions_Wacky/js/Expressions_wacky.js
+++ GoToTrainingWeek2/Expressions/Expressions_Wacky/js/Expressions_wacky.js
@@ -13,21 +13,42 @@ var yellowSkittle = 35;
 var redSkittle = 162;
 var greenSkittle = 150;
 
-var  yellowEaten = prompt("Hello!" +" " + "Lets calculate how many skittles are left"
-    + "Please enter how many yellow skittles you have eaten");
+var  yellowEaten = prompt("Hello!" +" " + "Lets calculate how many skittles are left."
+    + " " + "Please enter how many yellow skittles you have eaten.");
+
+ //check prompt in console
  console.log(yellowEaten);
-var redEaten = prompt("please enter how many red you have eaten");
-console.log(redEaten);
-var greenEaten = prompt("Now enter how many green you have eaten");
+var redEaten = prompt("Please enter how many red skittles you have eaten.");
 
-//set up variables for equation
-var yellowLeft = (yellowSkittle - yellowEaten);
-var redLeft = (redSkittle - redEaten);
-var greenLeft = (greenSkittle - greenEaten);
+ //check prompt in console
+ console.log(redEaten);
+ var greenEaten = prompt("Now enter how many green skittles you have eaten.");
 
-alert ("Ok there was "+yellowLeft+"  yellow skittles leftover and "+redLeft+" red skittles left over" + "and there was " +
- "+greenLeft+" "green skittles left over".);
-console.log("Ok there was "+yellowLeft+"  yellow skittles leftover and "+redLeft+" red skittles left over" + "and there was " +
-    "+greenLeft+" "green skittles left over".);
- //alert("Testing 1, 2, 3!");
-//alert("Testing 1, 2, 3!");
+ //check variable in console
+ console.log(greenEaten);
+
+
+ //set up variables for equation
+ var yellowLeft = (yellowSkittle - yellowEaten);
+ console.log(yellowLeft);
+// set up another variable for equation
+ var redLeft = (redSkittle - redEaten);
+ console.log(redLeft);
+
+ var greenLeft = (greenSkittle - greenEaten);
+ console.log(greenLeft);
+
+ alert("Ok! There was "+yellowLeft+" yellow skittles left, "+redLeft+" red skittles left and "+greenLeft+" green" +
+     "skittles left.");
+
+ //Printed alert to console
+ console.log("Ok! There was "+yellowLeft+" yellow skittles left, "+redLeft+" red skittles left and "+greenLeft+" green" +
+    "skittles left.");
+
+
+
+
+
+
+
+
