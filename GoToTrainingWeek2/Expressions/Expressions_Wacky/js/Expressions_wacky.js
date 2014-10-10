b1ac67d6d2de6cb65b6085de496586902a455c0a
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-09 23:58:30 -0400

    prompted user and alerted the user
    setup alert and prompted user

--- GoToTrainingWeek2/Expressions/Expressions_Wacky/js/Expressions_wacky.js
+++ GoToTrainingWeek2/Expressions/Expressions_Wacky/js/Expressions_wacky.js
@@ -20,7 +20,14 @@ var redEaten = prompt("please enter how many red you have eaten");
 console.log(redEaten);
 var greenEaten = prompt("Now enter how many green you have eaten");
 
+//set up variables for equation
 var yellowLeft = (yellowSkittle - yellowEaten);
 var redLeft = (redSkittle - redEaten);
 var greenLeft = (greenSkittle - greenEaten);
+
+alert ("Ok there was "+yellowLeft+"  yellow skittles leftover and "+redLeft+" red skittles left over" + "and there was " +
+ "+greenLeft+" "green skittles left over".);
+console.log("Ok there was "+yellowLeft+"  yellow skittles leftover and "+redLeft+" red skittles left over" + "and there was " +
+    "+greenLeft+" "green skittles left over".);
+ //alert("Testing 1, 2, 3!");
 //alert("Testing 1, 2, 3!");
