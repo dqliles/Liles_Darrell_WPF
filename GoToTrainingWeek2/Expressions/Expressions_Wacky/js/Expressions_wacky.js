b2b362555115f3bb92dffe81a4f513408d2889e4
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-09 23:36:15 -0400

    setup the given variables
    setup the given variables

--- GoToTrainingWeek2/Expressions/Expressions_Wacky/js/Expressions_wacky.js
+++ GoToTrainingWeek2/Expressions/Expressions_Wacky/js/Expressions_wacky.js
@@ -8,5 +8,11 @@ Expressions_Wacky
 
 //Calculate how many skittles were eaten
 
+//setup the given variables
+var yellowSkittle = 5;
+var redSkittle = 10;
+var greenSkittle = 15;
+
+
 
 alert("Testing 1, 2, 3!");
