88462e53f341f44aaf1865f395c2377c8a4c173b
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-16 18:23:49 -0400

    complete
    completed setting up conditionals

--- GoToTrainingWeek3/Conditionals/Conditionals_Personal/js/week3.js
+++ GoToTrainingWeek3/Conditionals/Conditionals_Personal/js/week3.js
@@ -2,7 +2,7 @@
  /*
 Darrell Liles
 Oct 16 2014
-WPF Section 02
+Conditionals_Personal
 */
 
 
@@ -13,7 +13,7 @@ WPF Section 02
 //setup the given variables
 var firstAndLastName = "Darrell"+" Liles";
 //tested variable setup in console
-console.log(firstAndLastName);
+console.log(firstAndLastName); //Tested in the console
 var email = "dqliles@fullsail.edu";
 console.log(email);
 
@@ -28,14 +28,17 @@ console.log(nameInputByUser);
         console.log (blankEntryForName);  //Tested in console
  }else if(nameInputByUser=== firstAndLastName){  //True?
      emailInputByUser = prompt("Now Please enter your email address."); //
-        console.log(emailInputByUser);
- }else{ wrongNameEntry = prompt("The name you have entered does not match our records."+" Please re-enter you first and last name.")
-        console.log(wrongNameEntry);
+        console.log(emailInputByUser);  //tested out in console
+ }else{ wrongNameEntry = prompt("The name you have entered does not match our records."+" Please re-enter your first and last name.")
+        console.log(wrongNameEntry); //Tested in console
 
  }
-
+ //Setup up condition for info collected from prompt
 if (emailInputByUser === email){
     PasswordResetSuccess = prompt("Great!"+" A link was sent to your email address to reset your password.");
+
+}else{wrongEmailEntry = prompt ("The email you have entered does not match our records."+" Please re-enter your email.");
+    console.log(wrongEmailEntry);
 }
 
 
@@ -57,7 +60,8 @@ if (emailInputByUser === email){
 
 
 
- //setup for the second prompt
+
+
 
 
 
