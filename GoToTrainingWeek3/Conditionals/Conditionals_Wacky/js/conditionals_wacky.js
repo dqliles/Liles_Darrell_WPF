406d417bb640c97b7d0a1cdc224f38a2e93a7217
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-16 22:56:01 -0400

    setup given variables
    setup given variables

--- GoToTrainingWeek3/Conditionals/Conditionals_Wacky/js/conditionals_wacky.js
+++ GoToTrainingWeek3/Conditionals/Conditionals_Wacky/js/conditionals_wacky.js
@@ -7,11 +7,18 @@ Conditionals_Wacky
 
 
 //We are calculating for a wacky photo store. The deal for today is the more pics you bring in to get developed,
-//the cheaper the price will be. 
+//the cheaper the price will be.
 
+//setup givens for photo prices
 
+ var TwentyPhotosOrMore = 20;
+    console.log(TwentyPhotosOrMore);
+ var FiftyPhotosOrMore = 15;
+    console.log(FiftyPhotosOrMore);
+ var oneHundredPhotosOrMore = 8;
+  console.log(oneHundredPhotosOrMore);
 
-
+ var ticketsInput = prompt("Welcome!"+ " Please enter the number of photos you will be bringing in.");
 
 
 
