d742d4589d6a094c4485c1a03ae1a44b6b48b430
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-16 20:25:32 -0400

    Setup prompt to obtain age from user
    Setup prompt to obtain age from user

--- GoToTrainingWeek3/Conditionals/Conditionals_Industry/js/conditionals_industry.js
+++ GoToTrainingWeek3/Conditionals/Conditionals_Industry/js/conditionals_industry.js
@@ -7,5 +7,18 @@ Conditionals_Industry
 
 
 
- 
-alert("Testing 1, 2, 3!");
+ //Fitness club down town wants to track the age of people that are trying to become members. Only
+// 18 and older are allowed to join, and senior citizens older than 50 get a 20% discount.
+
+
+//Set up given variables
+var minimumAgeToJoin = 18;
+ console.log(minimumAgeToJoin);
+var discountAge =50;
+ console.log(discountAge);
+
+//Setup Prompt to obtain age from user
+var ageInput = prompt("Thank you for your interest in our facility!" +" Before we continue registration we must verify that you are at least 18."+" Please enter your age.");
+    console.log(ageInput);
+
+//alert("Testing 1, 2, 3!");
--- GoToTrainingWeek3/Conditionals/Conditionals_Industry/js/week3.js
+++
@@ -1,9 +0,0 @@
-
- /*
-Darrell Liles
-Oct 07 2014
-WPF Section 02
-
- */
-
-alert("Testing 1, 2, 3!");
