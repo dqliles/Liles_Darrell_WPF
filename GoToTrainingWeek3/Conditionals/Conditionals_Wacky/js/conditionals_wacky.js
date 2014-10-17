e236b244bffd76b243b914354199695af70ea8ca
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-16 22:29:37 -0400

    setup files for Conditionals_Wacky
    setup JS script with HTML

---
+++ GoToTrainingWeek3/Conditionals/Conditionals_Wacky/js/conditionals_wacky.js
@@ -0,0 +1,44 @@
+
+ /*
+Darrell Liles
+Oct 07 2014
+Conditionals_Wacky
+ */
+
+
+//We are calculating for a wacky photo store. The deal for today is the more pics you bring in to get developed,
+//the cheaper the price will be. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert("Testing 1, 2, 3!");
