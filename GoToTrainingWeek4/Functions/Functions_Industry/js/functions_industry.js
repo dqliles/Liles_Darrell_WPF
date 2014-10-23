aede2e936b9f3b1594a5c944923fa63916de9998
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-23 18:52:04 -0400

    setup variable
    setup variable to collect info from prompt

--- GoToTrainingWeek4/Functions/Functions_Industry/js/functions_industry.js
+++ GoToTrainingWeek4/Functions/Functions_Industry/js/functions_industry.js
@@ -9,14 +9,14 @@ WPF Section 02
 
  //calculate the cost of chocolate,lemon, and strawberry cake for Thanksgiving
 
-
+//Setup variable to collect info from prompt
  var strawberryCake = prompt("Hello!" + " Please enter the number of strawberry cakes you will need.");
     console.log(strawberryCake);
 
         while(isNan(strawberryCake) || strawberryCake===""){
 
             strawberryCake = prompt("Please enter the number of strawberry cakes you will need");
-                
+
         }
 
 
