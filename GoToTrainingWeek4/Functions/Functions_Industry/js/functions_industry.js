9cfc190f481d1ff817096e795faaf53e849f3edb
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-23 18:50:19 -0400

    setup files
    setup JS file to interact with HTML

---
+++ GoToTrainingWeek4/Functions/Functions_Industry/js/functions_industry.js
@@ -0,0 +1,34 @@
+
+ /*
+Darrell Liles
+Oct 23 2014
+WPF Section 02
+
+ */
+
+
+ //calculate the cost of chocolate,lemon, and strawberry cake for Thanksgiving
+
+
+ var strawberryCake = prompt("Hello!" + " Please enter the number of strawberry cakes you will need.");
+    console.log(strawberryCake);
+
+        while(isNan(strawberryCake) || strawberryCake===""){
+
+            strawberryCake = prompt("Please enter the number of strawberry cakes you will need");
+                
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert("Testing 1, 2, 3!");
