e88c3e412e4ccec218f2ad864a937d6f1153a57b
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-23 22:07:27 -0400

    setup given variables
    setup given variables

--- GoToTrainingWeek4/Functions/Functions_Industry/js/functions_industry.js
+++ GoToTrainingWeek4/Functions/Functions_Industry/js/functions_industry.js
@@ -3,10 +3,33 @@
 Darrell Liles
 Oct 23 2014
 Functions_Industry
+*/
+
+
+
+// A store manager in an shoe department store wants to keep track of how much of their products are being bought and how much is left for his inventory.
+// This will give the store manager an indication of what needs to be ordered.
+
+ var jordanSneakersLeft = 24; //  the amount of Jordan sneakers left for inventory
+ console.log(jordanSneakersLeft); // tested variable for jordan sneakers in console
+ var lebronSneakersLeft =50;// amount of Lebron sneakers left
+ console.log(lebronSneakersLeft);// tested in console
 
 
 
 
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //alert("Testing 1, 2, 3!");
