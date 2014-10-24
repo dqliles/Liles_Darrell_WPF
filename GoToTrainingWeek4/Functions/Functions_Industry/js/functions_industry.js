d217a0d6d2a427903f621a911d684b4cc5718955
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-23 22:22:07 -0400

    setup validation with a while loop
    setup validation with a while loop

--- GoToTrainingWeek4/Functions/Functions_Industry/js/functions_industry.js
+++ GoToTrainingWeek4/Functions/Functions_Industry/js/functions_industry.js
@@ -7,21 +7,22 @@ Functions_Industry
 
 
 
-// A store manager in an shoe department store wants to keep track of how much of their products are being bought and how much is left for his inventory.
-// This will give the store manager an indication of what needs to be ordered.
-
- var jordanSneakersLeft = 24; //  the amount of Jordan sneakers left for inventory
+// The new Jordan and Lebron sneakers just came out. People have been camped out all night to purchase them, and they are almost sold out.
+//You have to now purchase them online.
+ var jordanSneakersLeft = 4; //  the amount of Jordan sneakers left for inventory
  console.log(jordanSneakersLeft); // tested variable for jordan sneakers in console
- var lebronSneakersLeft =50;// amount of Lebron sneakers left
+ var lebronSneakersLeft =6;// amount of Lebron sneakers left
  console.log(lebronSneakersLeft);// tested in console
+
 
  //Setup prompt to obtain info from user
  var jordanPurchase = prompt("Thank you for your interest in our sneakers!"+" Please enter the number of Jordan sneakers you would like to purchase!");
  console.log(jordanPurchase);  //Tested prompt input in console
 
 
+ while(isNaN(jordanPurchase) || jordanPurchase==="") { //setup validation with a while loop
 
-
+ }
 
 
 
