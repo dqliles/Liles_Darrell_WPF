9f62d2763b11b267878c45a37422c3b7c00dbef9
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-23 20:12:39 -0400

    setup prompt and validation for lemon cake
    setup prompt and validation for lemon cake

--- GoToTrainingWeek4/Functions/Functions_Industry/js/functions_industry.js
+++ GoToTrainingWeek4/Functions/Functions_Industry/js/functions_industry.js
@@ -1,39 +1,9 @@
 
  /*
 Darrell Liles
-Oct 23 2014
+Oct 07 2014
 WPF Section 02
 
  */
 
-
- //calculate the cost of chocolate,lemon, and strawberry cake for Thanksgiving
-
-//Setup variable to collect info from prompt
- var strawberryCake = prompt("Hello!" + " Please enter the number of strawberry cakes you will need.");
-    console.log(strawberryCake);   //checked out prompt in console
-
-        while(isNaN(strawberryCake) || strawberryCake===""){ //setup validation with a while loop
-
-            strawberryCake = prompt("Please enter the number of strawberry cakes you will need");
-
-        }
-//setup second variable and prompt for chocolate cake
- var chocolateCake = prompt("Hello!" + " Please enter the number of chocolate cakes you will need.");
-
-        while(isNaN(strawberryCake) || strawberryCake===""){ //setup validation with a while loop
-
- }
-
-
-
-
-
-
-
-
-
-
-
-
-//alert("Testing 1, 2, 3!");
+alert("Testing 1, 2, 3!");
