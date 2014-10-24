4680ea2b6e1895f3467f6c2c686a152a22520493
dqliles <WEBDEVELOPER@darrells-mbp-2.home>
2014-10-23 23:44:47 -0400

    set up variables
    set up variables

--- GoToTrainingWeek4/Functions/Functions_Wacky/js/functions_wacky.js
+++ GoToTrainingWeek4/Functions/Functions_Wacky/js/functions_wacky.js
@@ -1,9 +1,25 @@
 
  /*
 Darrell Liles
-Oct 07 2014
-WPF Section 02
+Oct 23 2014
+Functions_Wacky
 
  */
 
-alert("Testing 1, 2, 3!");
+// we are calculating for a all you can eat charity event the more people you bring the cheaper the cost
+
+ //setup givens for photo prices
+
+ var tenPeopleOrMore = 10;
+ console.log(tenPeopleOrMore);// tested in console
+ var twentyPeopleOrMore = 20;
+ console.log(twentyPeopleOrMore);
+ var thirtyPeopleOrMore = 30;
+ console.log(thirtyPeopleOrMore);
+
+
+
+
+ 
+
+//alert("Testing 1, 2, 3!");
